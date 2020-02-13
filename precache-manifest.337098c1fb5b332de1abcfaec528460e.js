6f7d19a43d226601c7bd0005aa33fa73f7eee71e
Pramod <9kpramod@gmail.com>
2020-02-13 23:31:23 +0100

    Add files via upload

---
+++ precache-manifest.337098c1fb5b332de1abcfaec528460e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6d45ccf9740a8d9f3f1c9bf4d80fef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25f1ae13ab05e4e0543e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "088619bf335938853523",
+    "url": "/static/js/2.fef581b2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fef581b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f1ae13ab05e4e0543e",
+    "url": "/static/js/main.6472f240.chunk.js"
+  },
+  {
+    "revision": "53cd4f60f6a7bd66d08e",
+    "url": "/static/js/runtime-main.403e09d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
